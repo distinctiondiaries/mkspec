15c65162e63fe8acce1ef20240a657b36634cb1b
Ritchie Young <ritchiey@myediary.com.au>
2013-12-19 11:56:04 +0800

    Generate specs for methods with multiple args of various types

--- lib/mkspec.rb
+++ lib/mkspec.rb
@@ -20,16 +20,36 @@ describe #{@class_name} do
   describe "##{@method_name}" do
 
     it "does something" do
-      expect(subject.#{@method_name}("#{@args}")).to #{matcher}
+      expect(subject.#{@method_name}(#{@args.map{|arg| serialise(arg) }.join(', ')})).to #{matcher}
     end
 
   end
 end
       EOF
     end
+
+    # Attempt to recreate the source-code to represent this argument in the setup
+    # for our generated spec.
+    def serialise(arg)
+      case arg.class.name
+      when 'String'
+        %Q('#{arg}')
+      when 'Fixnum'
+        arg
+      else
+        guess_constructor arg
+      end
+    end
+
+    # Don't recognise the type so we don't know how to recreate it
+    # in source code. So we'll take a guess at what might work and
+    # let the user fix it up if necessary.
+    def guess_constructor(arg)
+      "#{arg.class.name}.new('#{arg.to_s}')"
+    end
   end
 
-  def expect(sut, method, args)
+  def expect(sut, method, *args)
     LeftHand.new(sut.class.name, method, args)
   end
 
